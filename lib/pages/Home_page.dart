c0536bb8e173aeb44ff49af1979568cbab668b9d
Pradeep <pawarpradeep1707@gmail.com>
2025-01-16 20:34:12 +0530

    App Bar done

--- lib/pages/Home_page.dart
+++ lib/pages/Home_page.dart
@@ -1,11 +1,44 @@
 import 'package:flutter/material.dart';
 
-
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        elevation: 0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Icon(
+              Icons.menu,
+              color: Colors.grey[100],
+            ),
+            Container(
+              width: 40,
+              height: 40,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(14),
+                image: DecorationImage(
+                  image: AssetImage('assests/images/P5.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+      body: Column(
+        children: [
+          Text(
+            "Find the best",
+            style: TextStyle(color: Colors.white),
+          )
+        ],
+      ),
+    );
   }
 }

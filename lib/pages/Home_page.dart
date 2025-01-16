1ec547ef59c1d62c96055439df4c5d52a92d9457
Pradeep <pawarpradeep1707@gmail.com>
2025-01-16 20:44:14 +0530

    replaced the contnet of app bar with the body

--- lib/pages/Home_page.dart
+++ lib/pages/Home_page.dart
@@ -7,37 +7,40 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        elevation: 0,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: SafeArea(
+        child: Column(
           children: [
-            Icon(
-              Icons.menu,
-              color: Colors.grey[100],
-            ),
-            Container(
-              width: 40,
-              height: 40,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(14),
-                image: DecorationImage(
-                  image: AssetImage('assests/images/P5.jpg'),
-                  fit: BoxFit.cover,
-                ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0, top: 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 15.0),
+                    child: Icon(
+                      Icons.menu,
+                      color: Colors.grey[100],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 30.0),
+                    child: Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(14),
+                        image: DecorationImage(
+                          image: AssetImage('assests/images/P5.jpg'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
               ),
-            )
+            ),
           ],
         ),
-      ),
-      body: Column(
-        children: [
-          Text(
-            "Find the best",
-            style: TextStyle(color: Colors.white),
-          )
-        ],
       ),
     );
   }

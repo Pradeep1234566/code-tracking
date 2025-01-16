8508428bd9cc6c9e57648c6203efea387621892b
Pradeep <pawarpradeep1707@gmail.com>
2025-01-16 20:59:03 +0530

    Made till serach engine

--- lib/pages/Home_page.dart
+++ lib/pages/Home_page.dart
@@ -39,6 +39,52 @@ class HomePage extends StatelessWidget {
                 ],
               ),
             ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Find the best",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 35,
+                      fontFamily: 'biennale'),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "coffee for you",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 35,
+                      fontFamily: 'biennale'),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.grey[800],
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: Colors.grey[400],
+                ),
+                hintText: 'Find your Coffee',
+                hintStyle: TextStyle(
+                  color: Colors.grey[400],
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            )
           ],
         ),
       ),
